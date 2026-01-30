56facc536df2de47eb2f7f624bfc876762aea21f
GuikiPT <foxzillapt17@gmail.com>
2026-01-30 15:16:08 +0000

    [UPDATE] Allowed GuikiPT Workspace to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [tailwindcss(), sveltekit()],
+    server: {
+        allowedHosts: ['5173--main--guikipt-portfolio--guikipt.coder.guiki.pt']
+    }
+});
